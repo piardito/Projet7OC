f50e272867205b9ee6ef54aa6c0d2439d23dd8c0
piardito <piardito38@gmail.com>
2023-01-15 10:27:01 +0100

    commit

--- HomeCreditDefaultRisk.py
+++ HomeCreditDefaultRisk.py
@@ -9,11 +9,7 @@ import plotly.graph_objects as go
 import requests
 import json
 
-headers={
 
-    "authorization":st.secrets("auth_key"),
-    "content_type": "application/json"
-}
 
 st.set_page_config(layout="wide")
 
